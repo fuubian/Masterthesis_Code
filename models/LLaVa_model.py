b7dd3237eb2e503f5d356f59ee030e062a887926
fuubian <fabiangehteuchnichtsan@gmail.com>
2025-02-14 19:00:11 +0100

    Update folder structure

--- models/LLaVa_model.py
+++ models/LLaVa_model.py
@@ -1,9 +1,9 @@
-from models import TextImageModel
+from models import text_image_model
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 from PIL import Image
 
-class LLaVa_model(TextImageModel):
+class LLaVa_model(text_image_model):
     def __init__(self, model_name="llava-hf/llava-1.5-13b"):
         super().__init__(model_name)
         self._load_model()
