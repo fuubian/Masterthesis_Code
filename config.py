db5f4ed6d3da0aed362048d21399c96a41e0c355
fuubian <fabiangehteuchnichtsan@gmail.com>
2025-03-20 19:50:12 +0100

    Update config file

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATASET_PATH = os.path.join(BASE_DIR, "data")
 
 # Paths to splits of the main QA-pair dataset
-QA_TEST_SPLIT_PATH = os.path.join(DATASET_PATH, "test_split.csv")
-QA_TRAIN_SPLIT_PATH = os.path.join(DATASET_PATH, "train_split.json")
+QA_TEST_SPLIT_PATH = os.path.join(DATASET_PATH, "task1_test_split.csv")
+QA_TRAIN_SPLIT_PATH = os.path.join(DATASET_PATH, "task1_train_split.json")
 QA_TEST_SPLIT_LATEX_CLEAN_PATH = os.path.join(DATASET_PATH, "latex_clean_test_split.csv")
 
 # Paths to cvs files for task 2 and 3
-QA_TEST_TASK2_PATH = os.path.join(DATASET_PATH, "qa_pairs_task2.csv")
-QA_TEST_TASK3_PATH = os.path.join(DATASET_PATH, "qa_pairs_task3.csv")
+QA_TEST_TASK2_PATH = os.path.join(DATASET_PATH, "task2.csv")
+QA_TEST_TASK3_PATH = os.path.join(DATASET_PATH, "task3.csv")
 
 # Metadata paths to csv file
 FIGURE_METADATA_PATH = os.path.join(DATASET_PATH, "figures.csv")
