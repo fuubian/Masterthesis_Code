6623a0616a3924646e1ef23e59159afcffc37542
fuubian <fabiangehteuchnichtsan@gmail.com>
2025-03-28 21:36:51 +0100

    Fix lower case for metric selection

--- utils/metric_loader.py
+++ utils/metric_loader.py
@@ -4,18 +4,18 @@ from metrics import bert_score, llm_accuracy, vqa_mqm, meteor, cider, bleu, roug
 class MetricLoader:
     @staticmethod
     def load_metric(metric_name):
-        if metric_name == config.LLM_ACCURACY_METRIC:
+        if metric_name.lower() == config.LLM_ACCURACY_METRIC:
             return llm_accuracy.LLMAccuracy()
-        if metric_name == config.VQA_MQM_METRIC:
+        if metric_name.lower() == config.VQA_MQM_METRIC:
             return vqa_mqm.VQA_MQM()
-        if metric_name == config.METEOR_METRIC:
+        if metric_name.lower() == config.METEOR_METRIC:
             return meteor.MeteorMetric()
-        if metric_name == config.BERT_SCORE_METRIC:
+        if metric_name.lower() == config.BERT_SCORE_METRIC:
             return bert_score.BertScoreMetric()
-        if metric_name == config.CIDER_METRIC:
+        if metric_name.lower() == config.CIDER_METRIC:
             return cider.CiderMetric()
-        if metric_name == config.BLEU_METRIC:
+        if metric_name.lower() == config.BLEU_METRIC:
             return bleu.BleuMetric()
-        if metric_name == config.ROUGE_METRIC:
+        if metric_name.lower() == config.ROUGE_METRIC:
             return rouge.RougeMetric()
         raise ValueError(f"Undefined metric name received: {metric_name}. Please check the metric list to identify the correct metric name.")
